574fe38f12d1aa4861525b275231fee3e9e8a354
aidan-lynch <60490249+aidan-lynch@users.noreply.github.com>
2020-02-13 14:40:55 -0800

    Update sketch.js

--- dotdrawing/sketch.js
+++ dotdrawing/sketch.js
@@ -1,10 +1,18 @@
+let button;
 function setup() {
   createCanvas(400, 400);
   background(220);
+      button = createButton('reset');
+  button.position(width-50,height-25);
+  button.mousePressed(reset);
 }
 function draw() {
   ellipse(mouseX,mouseY,20,20)
   if (mouseIsPressed) {
     fill(random(255),random(255),random(255))
   }
+}
+function reset() {
+clear()
+background(220)
 }
