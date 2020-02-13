e93381475bfd99ddd896377401beb17bb6539eb6
aidan-lynch <60490249+aidan-lynch@users.noreply.github.com>
2020-02-13 14:42:32 -0800

    Update sketch.js

--- shapes/sketch.js
+++ shapes/sketch.js
@@ -2,18 +2,15 @@ let button;
 function setup() {
   createCanvas(800, 400);
   background(185, 250, 222)
-    button = createButton('reset');
+  button = createButton('reset');
   button.position(width-50,height-25);
   button.mousePressed(reset);
 }
-
 function draw() {
   if (mouseIsPressed) {
-    fill (242, 184, 234);
-  } else {
-    fill (243, 247, 178);
+    fill(random(255),random(255),random(255));
   }
-  rect(mouseX,mouseY,100,50, 20);
+  ellipse(mouseX,mouseY,50,50,20);
 }
 function reset() {
 clear()
