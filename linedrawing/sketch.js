856bb01d1ba813a0777eab549d8f7ec903e67105
aidan-lynch <60490249+aidan-lynch@users.noreply.github.com>
2020-02-13 14:39:36 -0800

    Update sketch.js

--- linedrawing/sketch.js
+++ linedrawing/sketch.js
@@ -1,6 +1,10 @@
+let button;
 function setup() {
   createCanvas(400, 400);
   background(220);
+    button = createButton('reset');
+  button.position(width-50,height-25);
+  button.mousePressed(reset);
 }
 function draw() {
   if (mouseIsPressed) {
@@ -8,4 +12,8 @@ function draw() {
      line(200,200,mouseX,mouseY)
   ellipse(mouseX,mouseY,20,20)
   }
+}
+function reset() {
+clear()
+background(220)
 }
