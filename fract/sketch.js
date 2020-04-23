97b947fe3571da64c259252f4990c49283fd2ea1
aidan-lynch <60490249+aidan-lynch@users.noreply.github.com>
2020-04-23 09:53:45 -0700

    Add files via upload

---
+++ fract/sketch.js
@@ -0,0 +1,30 @@
+let slider;
+function drawSquare(x, y, s) {
+  fill(random(255),random(255),random(255));
+  rect(x, y, s, s);
+  if (s > 2) {
+    fill(random(255),random(255),random(255));
+    drawSquare(x + s / 2, y*2, s / 2);
+    drawSquare(x - s / 2, y, s / 2);
+    drawSquare(x, y+s/2, s/2);
+    drawSquare(x, y+s*2, s/2);
+  }
+}
+function setup() {
+  createCanvas(600, 600);
+  slider = createSlider(0, 255, 100);
+  slider.position(10, 10);
+  slider.style('width', '80px');  
+}
+function draw() {
+    background(220);
+  noFill();
+  drawSquare(width / 2, height / 3, 200);
+    let n = slider.value();
+  fill(0)
+  textSize(20)
+  text('n =',50,50);
+  text(n,85,50);
+  text('n! =',50,70);
+  text(n*(n-1),90,70);
+}
